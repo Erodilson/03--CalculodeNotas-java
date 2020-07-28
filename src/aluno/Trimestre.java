3abbb25d32156bbb7292f506e31df801ac3b49f1
Erodilson c souza <erodilsoncqcv@gmail.com>
2020-07-28 13:54:47 -0300

    Escola

---
+++ src/aluno/Trimestre.java
@@ -0,0 +1,47 @@
+package aluno;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import atividades.ResultadoFinal;
+
+public class Trimestre {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+       
+        ResultadoFinal cont = new ResultadoFinal ();
+        
+     System.out.println("Digite o nome do aluno:");
+     
+        cont.nome =sc.nextLine();
+        
+     System.out.println("Digite as notas trimestrais do aluno :"); 
+     
+     System.out.print("Primeiro trimestre:");
+       cont.tri1 =sc.nextDouble();
+       
+     System.out.print("Segundo trimestre:");
+       cont.tri2 =sc.nextDouble();
+      
+       System.out.print("Terceiro trimestre:");
+       cont.tri3 =sc.nextDouble();
+       System.out.println();
+     System.out.println(" A nota final deste aluno é :" +(cont.notafinal())  ); 
+       
+        if (cont.notafinal()<60)  {System.out.println("aluno reprovado" +"falta " +(100 - cont.notafinal() )+(" pontos"));
+        }
+        
+        else if (cont.notafinal()<=0){System.out.print("Você não compareceu as aulas :"+ (" falta ")+ ( 100- cont.notafinal())+ ("pontos"));
+        	
+        }
+        else { System.out.println("Você foi aprovado");
+        
+        
+        sc.close();
+	}
+
+	}
+	
+}
---
+++ src/atividades/ResultadoFinal.java
@@ -0,0 +1,15 @@
+package atividades;
+
+public class ResultadoFinal {
+
+	
+public String nome;
+public double tri1,tri2,tri3;
+
+public double notafinal(){
+	
+ return (tri1+tri2+tri3 );
+}
+	
+	
+}
