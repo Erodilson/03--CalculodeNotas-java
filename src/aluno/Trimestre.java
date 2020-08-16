655657850f678f8f54f192b9485c959a9b10a3d6
Erodilson C.Souza <erodilsoncqcv@gmail.com>
2020-08-15 21:06:28 -0300

    acrescentei os metodos getters e setters

--- src/aluno/Trimestre.java
+++ src/aluno/Trimestre.java
@@ -12,31 +12,32 @@ public class Trimestre {
         Scanner sc = new Scanner(System.in);
        
         ResultadoFinal cont = new ResultadoFinal ();
-        
+       
      System.out.println("Digite o nome do aluno:");
-     
-        cont.nome =sc.nextLine();
+   
+     cont.setNome(sc.nextLine());
+        
         
      System.out.println("Digite as notas trimestrais do aluno :"); 
      
      System.out.print("Primeiro trimestre:");
-       cont.tri1 =sc.nextDouble();
+       cont.setTri1(sc.nextDouble());
        
      System.out.print("Segundo trimestre:");
-       cont.tri2 =sc.nextDouble();
+       cont.setTri2 (sc.nextDouble());
       
        System.out.print("Terceiro trimestre:");
-       cont.tri3 =sc.nextDouble();
+       cont.setTri3 (sc.nextDouble());
        System.out.println();
      System.out.println(" A nota final deste aluno é :" +(cont.notafinal())  ); 
        
-        if (cont.notafinal()<60)  {System.out.println("Aluno reprovado" +" falta " +(100 - cont.notafinal() )+(" pontos"));
+        if (cont.notafinal()<60)  {System.out.println(cont.getNome()+" você foi reprovado" +" falta " +(100 - cont.notafinal() )+(" pontos"));
         }
         
-        else if (cont.notafinal()>100){System.out.print("Você superou as espectativas:"+ (" teve ")+ (  cont.notafinal())+ (" pontos"));
+        else if (cont.notafinal()>100){System.out.print(cont.getNome()+" você superou as espectativas:"+ (" teve ")+ (  cont.notafinal())+ (" pontos"));
         	
         }
-        else { System.out.println("Você foi aprovado");
+        else { System.out.println(cont.getNome()+" você foi aprovado");
         
         
         sc.close();
--- src/atividades/ResultadoFinal.java
+++ src/atividades/ResultadoFinal.java
@@ -3,13 +3,44 @@ package atividades;
 public class ResultadoFinal {
 
 	
-public String nome;
-public double tri1,tri2,tri3;
+private String nome;
+private double tri1,tri2,tri3;
 
 public double notafinal(){
 	
  return (tri1+tri2+tri3 );
 }
 	
-	
+
+public String getNome(){
+	return nome;
 }
+
+	public void setNome(String nome){
+		this.nome = nome;
+
+	}
+	public double getTri1(){
+		return tri1;
+	}
+	
+	public void setTri1(double tri1){
+		this.tri1 = tri1;
+	}
+	public double getTri2(){
+		return tri2;
+	}
+	
+	public void setTri2(double tri2){
+		this.tri2 = tri2;
+	}
+	public double getTri3(){
+		return tri3;
+	}
+	
+	public void setTri3(double tri3){
+		this.tri3 = tri3;
+	}
+}
+
+
